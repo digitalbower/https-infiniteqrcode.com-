ce1952f4d876470063b8372e903c40b5f22cb5ce
anjalidb345 <anjali@digitalbower.com>
2025-02-13 17:50:23 +0530

    Updated preview pages with map

--- resources/views/url-preview.blade.php
+++ resources/views/url-preview.blade.php
@@ -20,7 +20,7 @@
       <p class="pt-4 text-lg font-semibold text-indigo-200">Loading...</p>
     </div>
   </div>
-  
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBX1r_54BQXiGiu-6YOITWJ4UR3NAqbUyM"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
     $(document).ready(function() {
